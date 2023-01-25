cbefd9697cb7a6e31c5806a1e821924fddbca264
alex reinoso <34074669+aReinoso007@users.noreply.github.com>
2023-01-25 00:20:38 -0500

    event handler

--- README.md
+++ README.md
@@ -1,4 +1,12 @@
 # Picture getter
 
-## About
+## About 🔎
 Fetching data from an API that shows pictures related to user input
+
+## Event handlers and callbacks 🥽
+### onClick
+Whenever an user clicks on something
+### onChange
+User changes text in an input
+### onSubmit
+User submits a form
--- src/components/SearchBar.js
+++ src/components/SearchBar.js
@@ -5,14 +5,21 @@ Here we will use a class component to be able to use state for user input
 */
 class SearchBar extends React.Component {
 
-
+    onInputChange(event) {
+        console.log(event.target.value)
+    }
+    /*
+    we dont add the () on this.onInputChange bc we dont want it to be called
+    when the component is first rendered, so we only pass it the reference bc its an event
+    handler with callbacks
+    */
     render() {
         return (
             <div className="ui segment">
                 <form className="ui form">
                     <div className="field">
                         <label>Image Search</label>
-                        <input type="text"></input>
+                        <input type="text" onChange={this.onInputChange}></input>
                     </div>
                 </form>
             </div>
