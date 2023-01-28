443bfcd40bbf319a0727e6bc996fcb7f04d329fd
alex reinoso <34074669+aReinoso007@users.noreply.github.com>
2023-01-27 23:23:49 -0500

    data fetching and assigning

--- src/components/App.js
+++ src/components/App.js
@@ -4,21 +4,25 @@ import SearchBar from "./SearchBar";
 
 class App extends React.Component {
 
-    onSearchSubmit(term) {
-        axios.get('https://api.unsplash.com/search/photos', {
+    state = { images: [] };
+
+    onSearchSubmit = async (term) => {
+        const response = await axios.get('https://api.unsplash.com/search/photos', {
             params: {
                 query: term
             },
             headers: {
                 Authorization: 'Client-ID MJC-2pJ5_eX8nToQ5BT26z_3ZJX49TOjGK_WJ_mhJbo'
-            },
-        })
+            }
+        });
+        this.setState({ images: response.data.results });
     }
 
     render() {
         return (
             <div className="ui container" style={{ marginTop: '30px' }}>
                 <SearchBar onSubmit={this.onSearchSubmit} />
+                Found: {this.state.images.length} images
             </div>
         );
     }
